4a0397cbaacb263ecad413f13a861a9cc7239220
Junior Martins <amjr.box@gmail.com>
2021-10-11 18:00:14 -0300

    Testes dos componentes Logo e Header

---
+++ src/components/logo/logo.test.tsx
@@ -0,0 +1,9 @@
+import { LogoImg } from './logo-styled'
+import LogoTimeSheet from './logo.svg'
+import { Logo } from './logo'
+
+describe('header', () => {
+  it('deve retornar o logo', () => {
+    expect(Logo()).toEqual(<LogoImg src={LogoTimeSheet} alt="Timesheet" />)
+  })
+})
--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,22 +1,8 @@
-import { LogoBox, LogoImg } from './logo-styled'
+import { LogoImg } from './logo-styled'
 import LogoTimeSheet from './logo.svg'
 
 function Logo() {
-  function handleClick() {
-    window.history.pushState(null, '', '/')
-  }
-
-  return (
-    <LogoBox>
-      <LogoImg
-        src={LogoTimeSheet}
-        alt="Timesheet"
-        onClick={() => {
-          handleClick()
-        }}
-      />
-    </LogoBox>
-  )
+  return <LogoImg src={LogoTimeSheet} alt="Timesheet" />
 }
 
 export { Logo }
