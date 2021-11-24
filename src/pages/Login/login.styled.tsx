f90766afc21e1765c001e1d8628b829b7f262b8d
Junior Martins <amjr.box@gmail.com>
2021-11-24 07:55:26 -0300

    Start Component Login

---
+++ src/pages/Login/login.styled.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components/macro'
+
+const BoxLogin = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 20px;
+  width: 300px;
+  height: 300px;
+  background-color: #fff;
+  box-shadow: 0px 0px 10px #ccc;
+`
+
+export { BoxLogin }
