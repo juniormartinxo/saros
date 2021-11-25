30b267fad0a27a6904f5a80faf9b6a43e3d0de24
Junior Martins <amjr.box@gmail.com>
2021-11-25 07:52:44 -0300

    Added function that saves in localStorage and finalization of the context

--- src/context/AuthProvider/index.tsx
+++ src/context/AuthProvider/index.tsx
@@ -1,11 +1,19 @@
-import { createContext, useState } from 'react'
+import { createContext, useEffect, useState } from 'react'
 import { IAuthProvider, IContext, IUser } from './types'
-import { LoginRequest } from './util'
+import { getUserLocalStorage, LoginRequest, setUserLocalStorage } from './util'
 
 export const AuthContext = createContext<IContext>({} as IContext)
 
 export const AuthProvider = ({ children }: IAuthProvider) => {
   const [user, setUser] = useState<IUser | null>()
+
+  useEffect(() => {
+    const user = getUserLocalStorage()
+
+    if (user) {
+      setUser(user)
+    }
+  }, [])
 
   async function authenticate(email: string, password: string) {
     const response = await LoginRequest(email, password)
@@ -13,10 +21,12 @@ export const AuthProvider = ({ children }: IAuthProvider) => {
     const payload = { token: response.token, email }
 
     setUser(payload)
+    setUserLocalStorage(payload)
   }
 
   function logout() {
     setUser(null)
+    setUserLocalStorage(null)
   }
 
   return (
--- src/context/AuthProvider/util.ts
+++ src/context/AuthProvider/util.ts
@@ -1,4 +1,21 @@
 import { Api } from 'services/api'
+import { IUser } from './types'
+
+export function setUserLocalStorage(user: IUser | null) {
+  localStorage.setItem('user', JSON.stringify(user))
+}
+
+export function getUserLocalStorage() {
+  const json = localStorage.getItem('USER')
+
+  if (!json) {
+    return null
+  }
+
+  const user = JSON.parse(json)
+
+  return user ?? null
+}
 
 export async function LoginRequest(email: string, password: string) {
   try {
